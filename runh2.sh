634267095966ce18e087274a9471e8c2e2d91654
Max Rydahl Andersen <max@slowbeard.local>
2012-03-07 11:44:12 +0100

    use proper way to prevent browser

--- runh2.sh
+++ runh2.sh
@@ -1,2 +1,2 @@
 echo Starting h2 server - jdbc url: jdbc:h2:tcp://localhost/sakila.db
-java -cp h2-1.3.161.jar -Dh2.browser=none org.h2.tools.Server -ifExists
+java -cp h2-1.3.161.jar org.h2.tools.Server -ifExists -tcp -web
