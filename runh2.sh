266bb07c7d235278a3afaf71758d99c374981643
Gavin King <gavin@hibernate.org>
2025-05-23 18:21:27 +0200

    update to h2 2.3

--- runh2.sh
+++ runh2.sh
@@ -1,2 +1,2 @@
 echo Starting h2 server - jdbc url: jdbc:h2:tcp://localhost/./sakila
-java -cp h2-1.4.200.jar org.h2.tools.Server -ifExists -tcp -web
+java -cp h2-2.3.232.jar org.h2.tools.Server -ifNotExists -tcp -web
