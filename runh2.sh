af3c3162aba3d0c951e93d819b214177620d027a
Koen Aers <koen.aers@gmail.com>
2025-10-22 11:02:39 +0200

    Update H2 to version 2.4.240
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- runh2.sh
+++ runh2.sh
@@ -14,4 +14,4 @@
 #  limitations under the License.
 
 echo Starting h2 server - jdbc url: jdbc:h2:tcp://localhost/./sakila
-java -cp h2-2.3.232.jar org.h2.tools.Server -ifNotExists -tcp -web
+java -cp h2-2.4.240.jar org.h2.tools.Server -ifNotExists -tcp -web
